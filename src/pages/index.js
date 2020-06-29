85d917068ebe717e1297fe4ac4a871849afd7f27
“Jules <julessaysrawr@gmail.com>
2020-06-29 16:37:31 -0400

    feat: adding starter form using hooks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const [name, setName] = useState("")
+  const [results, setResults] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    // alert(`Name is ${name}`)
+    setResults(name)
+  }
+
+  return (
+    <div>
+      {console.log('results: ', results)}
+      <h1>Basic Form Example</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={event => setName(event.target.value)}
+          />
+        </label>
+        <input type="submit" value="Submit 🚀"/>
+      </form>
+      <div>
+        <h4>Results</h4>
+        <div>{results}</div>
+      </div>
+    </div>
+  )
 }
