204a16e11111eb278e6d30f0224020a8d6d5caa4
“Jules <julessaysrawr@gmail.com>
2020-06-29 16:51:51 -0400

    refactor: moving basic example to it's own page

---
+++ src/pages/basic-example.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react"
+
+export default function BasicExample() {
+  const [name, setName] = useState("")
+  const [results, setResults] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    // alert(`Name is ${name}`)
+    setResults(name)
+  }
+
+  return (
+    <div>
+      {console.log('results: ', results)}
+      <h1>Basic Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={event => setName(event.target.value)}
+          />
+        </label>
+        <input type="submit" value="Submit 🚀"/>
+      </form>
+      <div>
+        <h4>Results</h4>
+        <div>{results}</div>
+      </div>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,12 @@
 import React, { useState } from "react"
+import { Link } from 'gatsby'
 
 export default function Home() {
-  const [name, setName] = useState("")
-  const [results, setResults] = useState('')
-
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    // alert(`Name is ${name}`)
-    setResults(name)
-  }
 
   return (
     <div>
-      {console.log('results: ', results)}
-      <h1>Basic Form Example</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">
-          Name
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={event => setName(event.target.value)}
-          />
-        </label>
-        <input type="submit" value="Submit 🚀"/>
-      </form>
-      <div>
-        <h4>Results</h4>
-        <div>{results}</div>
-      </div>
+      <h1>Form Examples</h1>
+      <Link to="/basic-example">Basic Example </Link>
     </div>
   )
 }
