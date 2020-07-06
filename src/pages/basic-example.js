05b5992201e113bd6e9e010121640f8ca08a4ac1
“Jules <julessaysrawr@gmail.com>
2020-07-06 16:22:36 -0400

    feat: adding checkboxes, radios & dropdown inputs

--- src/pages/basic-example.js
+++ src/pages/basic-example.js
@@ -5,12 +5,18 @@ import theme from "../theme"
 
 export default function BasicExample() {
   const [name, setName] = useState("")
+  // how to get an object of state?
   const [results, setResults] = useState("")
+  const [shirt, setShirt] = useState(false)
+  const [pants, setPants] = useState(false)
+  const [shoes, setShoes] = useState(false)
+  const [shipping, setShipping] = useState("")
+  const [packaging, setPackaging] = useState("")
 
   const handleSubmit = event => {
     event.preventDefault()
-    // alert(`Name is ${name}`)
-    setResults(name)
+    alert(`Nothing to see here`)
+    // setResults(option2)
   }
 
   return (
@@ -43,6 +49,109 @@ export default function BasicExample() {
             `}
           />
         </label>
+        <div
+          css={css`
+            margin: ${theme.space[4]}px 0;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+            width: calc(${theme.space[8]}px * 1.5);
+            // width: 300px;
+            // height: calc(${theme.space[6]}px * 1.5);
+          `}
+        >
+          <fieldset
+            css={css`
+              margin-bottom: ${theme.space[5]}px;
+            `}
+          >
+            <legend>Pick an product</legend>
+            <label htmlFor="shirt">
+              <input
+                type="checkbox"
+                id="shirt"
+                value={shirt}
+                checked={shirt}
+                onChange={event => setShirt(event.target.checked)}
+              />
+              Shirt
+            </label>
+            <label htmlFor="pants">
+              <input
+                type="checkbox"
+                id="pants"
+                value={pants}
+                checked={pants}
+                onChange={event => setPants(event.target.checked)}
+              />
+              Pants
+            </label>
+            <label htmlFor="shoes">
+              <input
+                type="checkbox"
+                id="shoes"
+                value={shoes}
+                checked={shoes}
+                onChange={event => setShoes(event.target.checked)}
+              />
+              Shoes
+            </label>
+          </fieldset>
+          <fieldset
+            css={css`
+              margin-bottom: ${theme.space[5]}px;
+            `}
+          >
+            <legend>Choose a shipping method:</legend>
+            <label htmlFor="overnight">
+              <input
+                type="radio"
+                name="shippiing"
+                id="overnight"
+                value="overnight"
+                checked={shipping === "overnight"}
+                onChange={event => setShipping(event.target.value)}
+              ></input>
+              Overnight
+            </label>
+            <label htmlFor="twoday">
+              <input
+                type="radio"
+                name="shippiing"
+                id="twoday"
+                value="twoday"
+                checked={shipping === "twoday"}
+                onChange={event => setShipping(event.target.value)}
+              ></input>
+              Two Day
+            </label>
+            <label htmlFor="ground">
+              <input
+                type="radio"
+                name="shippiing"
+                id="ground"
+                value="ground"
+                checked={shipping === "ground"}
+                onChange={event => setShipping(event.target.value)}
+              ></input>
+              Ground
+            </label>
+          </fieldset>
+          <label htmlFor="packaging">
+            What type of packaging do your prefer:
+            <select
+              id="packaging"
+              name="packaging"
+              value={packaging}
+              onChange={event => setPackaging(event.target.value)}
+            >
+              <option value="paper">Paper Only</option>
+              <option value="plastic">Plastic Okay (-$1)</option>
+              <option value="dontcare">I don't care</option>
+            </select>
+          </label>
+        </div>
+
         <input
           type="submit"
           value="Submit 🚀"
@@ -53,17 +162,26 @@ export default function BasicExample() {
           `}
         />
       </form>
-      <div>
+      <div
+        css={css`
+          margin-top: 200px;
+        `}
+      >
         <h4>Results</h4>
         <div
           css={css`
             border: 1px solid black;
-            height: 200px;
-            width: 300px;
+            width: calc(${theme.space[8]}px * 1.5);
             margin-bottom: 50px;
           `}
         >
-          {results}
+          {/* {results} */}
+          <p>name: {name}</p>
+          <p>shirt: {shirt.toString()}</p>
+          <p>pants: {pants.toString()}</p>
+          <p>shoes: {shoes.toString()}</p>
+          <p>Shipping: {shipping}</p>
+          <p>Packaging: {packaging}</p>
         </div>
       </div>
       <Link to="/">Back</Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import { Link } from 'gatsby'
-import { css } from "@emotion/core"
+// import { css } from "@emotion/core"
 
 export default function Home() {
 
